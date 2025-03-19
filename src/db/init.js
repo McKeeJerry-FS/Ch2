67ad6a6c52b85baeb5083c67cfd1337baf8d4ab7
McKeeJerry-FS <jerry.mckee.jr0882@gmail.com>
2025-03-18 20:32:54 -0400

    creating the new dependencies

--- src/db/init.js
+++ src/db/init.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export function initDatabase() {
-  const DATABASE_URL = process.env.DATABASE_URL
+  const DATABASE_URL = 'mongodb://localhost:27017/blog'
   mongoose.connection.on('open', () => {
     console.info('Connected to database', DATABASE_URL)
   })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import { app } from './app.js'
+import dotenv from 'dotenv'
+import { initDatabase } from './db/init.js'
 
-const PORT = 3000
-app.listen(PORT)
-console.info(`Express server running on http://localhost:${PORT}`)
+dotenv.config()
+
+try {
+  await initDatabase()
+  const PORT = process.env.PORT
+  app.listen(PORT)
+  console.info(`Express server running on http://localhost:${PORT}`)
+} catch (err) {
+  console.error('Error connecting to database', err)
+}
